390ab21384f498290aea4ef85c5341e164a93f5a
Nekorjak <nekorjak.michal@vlada.cz>
2022-01-12 18:57:37 +0100

    .

--- projekt1.py
+++ projekt1.py
@@ -1,8 +1,3 @@
-# V příkladu u zadání jsou výpočty, myslím, provedeny ne úplně ideálně. Nebo mám potíž porozumět zadání.
-# Např pro string "30N" není číslo započítáno, naopak písmeno N je patrně zahrnuto mezi slova začínající velkými
-# písmeny. Jiný příklad - do součtu slov(!) jsou započítána i čísla... Zvolil jsem přístup, který mi dává analyticky
-# větší smysl, ale v důsledku se budou lišit mé výpočty od výpočtů tvůrce zadání.
-
 from time import sleep
 import pprint
 
